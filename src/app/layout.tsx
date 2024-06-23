2a7097ff041bea94e547b8668222f732afd11af5
nighca <nighca@live.cn>
2024-06-23 20:11:36 +0800

    add manifest

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,7 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Wink - Wish bank',
   description: 'Created by nighca',
+  manifest: '/manifest.json'
 }
 
 export default function RootLayout({
