b12a4bf95eedf39f0e0e3e4753ef5c143c761d90
nighca <nighca@live.cn>
2024-07-17 21:35:26 +0800

    optimize amount input

--- src/app/group/[name]/AddDeal.tsx
+++ src/app/group/[name]/AddDeal.tsx
@@ -4,8 +4,9 @@ import { ChangeEvent, useRef, useState } from 'react'
 import { Button } from '@/components/ui/button'
 import { Deal, DetailedGroup, addDeal, useCurrentUser } from '@/models'
 import { Textarea } from "@/components/ui/textarea"
-import { Slider } from "@/components/ui/slider"
+import { Badge } from "@/components/ui/badge"
 import { Label } from '@/components/ui/label'
+import { Input } from '@/components/ui/input'
 
 const minAmount = 1
 const defaultAmount = 1
@@ -37,11 +38,11 @@ export default function AddDeal({ group, onAdded }: {
 
   const balance = user == null ? null : (group.balance[user.id] ?? null)
   const maxAmount = balance ?? 100
+  const quickAmounts = [1, 5, 10, 20, 50].filter(v => v <= maxAmount)
   const [amount, setAmount] = useState(defaultAmount)
-  function handleAmountChange([amount]: number[]) {
-    setAmount(amount)
+  function handleAmountChange(e: ChangeEvent<HTMLInputElement>) {
+    setAmount(e.target.valueAsNumber)
   }
-
   const [submitting, setSubmitting] = useState(false)
 
   async function handleAddDeal(e: React.FormEvent<HTMLFormElement>) {
@@ -65,14 +66,15 @@ export default function AddDeal({ group, onAdded }: {
       )}
       <form className={cns("grid gap-4", !editting && 'hidden')} onSubmit={handleAddDeal}>
         <Textarea ref={noteInput} name="note" disabled={submitting} placeholder="Note" required value={note} onChange={handleNoteChange} />
-        <div className='flex gap-2 items-center'>
+        <div className='flex gap-2 items-center justify-start'>
           <Label>$</Label>
-          <span className='text-xs text-slate-500'>{minAmount}</span>
-          <Slider disabled={submitting} min={minAmount} max={maxAmount} step={1} value={[amount]} onValueChange={handleAmountChange} />
-          <span className='text-xs text-slate-500'>{maxAmount}</span>
-          </div>
+          <Input type='number' disabled={submitting} min={minAmount} max={maxAmount} value={amount} onChange={handleAmountChange} />
+          {quickAmounts.map(v => (
+            <Badge key={v} variant="secondary" onClick={() => setAmount(v)}>{v}</Badge>
+          ))}
+        </div>
         <Button type="submit" className='w-full' loading={submitting}>
-          Deal ${amount}
+          Deal
         </Button>
         <Button type="button" variant="ghost" disabled={submitting} onClick={endEditting}>
           Cancel
